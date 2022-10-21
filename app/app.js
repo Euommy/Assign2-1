64e41753f132373c13dc0705295a0dbbda0a0f04
Euommy <104454973+Euommy@users.noreply.github.com>
2022-10-21 19:20:05 -0400

    Edit the server.js

--- app/app.js
+++ app/app.js
@@ -16,8 +16,8 @@ import mongoose from 'mongoose';
 import { MongoURI, Secret } from '../config/config.js';
 
 // Import Routes
-import indexRouter from './routes/index.route.server.js'
-
+import indexRouter from './routes/index.route.server.js';
+import movieRouter from './routes/movies.route.server.js';
 // Instantiate Express Application
 const app = express();
 
@@ -50,6 +50,7 @@ app.use(session({
 
 // Use Routes
 app.use('/', indexRouter);
+app.use('/', movieRouter);
 
 
 export default app;
